129f03e37fc3316a4df56ebfc86ebdc3ca5ff8c5
tytwitchell <tytwitchell@gmail.com>
2023-11-06 21:08:32 -0500

    refactor: icon tech spacing update

--- js/additional.js
+++ js/additional.js
@@ -1,29 +1,27 @@
 
-
 const observer = new IntersectionObserver((entries) => {
-    
-    entries.forEach((entry) => {
-        console.log(entry.target.classList)
-      
-        if (entry.isIntersecting && entry.target.classList.contains('hidden')) {
-            
-            entry.target.classList.add('show')
-        } else if (entry.isIntersecting && entry.target.classList.contains('hidden-txt')){
+  entries.forEach((entry) => {
+    console.log(entry.target.classList);
 
-            entry.target.classList.add('show-txt')
-        } else {
-            entry.target.classList.remove('show')
-            entry.target.classList.remove('show-txt')
-        }
-    })
-
-})
+    if (entry.isIntersecting && entry.target.classList.contains("hidden")) {
+      entry.target.classList.add("show");
+    } else if (
+      entry.isIntersecting &&
+      entry.target.classList.contains("hidden-txt")
+    ) {
+      entry.target.classList.add("show-txt");
+    } else {
+      entry.target.classList.remove("show");
+      entry.target.classList.remove("show-txt");
+    }
+  });
+});
 
 // const observer = new IntersectionObserver((entries) => {
-    
+
 //     entries.forEach((entry) => {
 //         console.log(entry)
-      
+
 //         if (entry.isIntersecting) {
 //             entry.target.classList.add('show')
 //         } else {
@@ -33,8 +31,8 @@ const observer = new IntersectionObserver((entries) => {
 
 // })
 
-const hiddenElements = document.querySelectorAll('.hidden')
-hiddenElements.forEach( (el) => observer.observe(el) )
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
 
-const hiddenText = document.querySelectorAll('.hidden-txt')
-hiddenText.forEach( (el) => observer.observe(el) )
+const hiddenText = document.querySelectorAll(".hidden-txt");
+hiddenText.forEach((el) => observer.observe(el));
